b294f2a47bfe568e2df78becaf561a96437f22fc
KomalSaini <komsaini@ix.lentra.ai>
2024-08-29 10:52:57 +0530

    Map application android code

---
+++ .gradle/8.2/dependencies-accessors/52056cba22eccd5b6ba236b0fd23a5f5b7ba9e0c/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,407 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final AndroidxLibraryAccessors laccForAndroidxLibraryAccessors = new AndroidxLibraryAccessors(owner);
+    private final MaplibreLibraryAccessors laccForMaplibreLibraryAccessors = new MaplibreLibraryAccessors(owner);
+    private final Okhttp3LibraryAccessors laccForOkhttp3LibraryAccessors = new Okhttp3LibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+        /**
+         * Creates a dependency provider for constraintlayout (androidx.constraintlayout:constraintlayout)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getConstraintlayout() {
+            return create("constraintlayout");
+    }
+
+        /**
+         * Creates a dependency provider for material (com.google.android.material:material)
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getMaterial() {
+            return create("material");
+    }
+
+    /**
+     * Returns the group of libraries at androidx
+     */
+    public AndroidxLibraryAccessors getAndroidx() {
+        return laccForAndroidxLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at maplibre
+     */
+    public MaplibreLibraryAccessors getMaplibre() {
+        return laccForMaplibreLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at okhttp3
+     */
+    public Okhttp3LibraryAccessors getOkhttp3() {
+        return laccForOkhttp3LibraryAccessors;
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class AndroidxLibraryAccessors extends SubDependencyFactory {
+        private final AndroidxCoreLibraryAccessors laccForAndroidxCoreLibraryAccessors = new AndroidxCoreLibraryAccessors(owner);
+        private final AndroidxFragmentLibraryAccessors laccForAndroidxFragmentLibraryAccessors = new AndroidxFragmentLibraryAccessors(owner);
+        private final AndroidxLifecycleLibraryAccessors laccForAndroidxLifecycleLibraryAccessors = new AndroidxLifecycleLibraryAccessors(owner);
+
+        public AndroidxLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for appcompat (androidx.appcompat:appcompat)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getAppcompat() {
+                return create("androidx.appcompat");
+        }
+
+        /**
+         * Returns the group of libraries at androidx.core
+         */
+        public AndroidxCoreLibraryAccessors getCore() {
+            return laccForAndroidxCoreLibraryAccessors;
+        }
+
+        /**
+         * Returns the group of libraries at androidx.fragment
+         */
+        public AndroidxFragmentLibraryAccessors getFragment() {
+            return laccForAndroidxFragmentLibraryAccessors;
+        }
+
+        /**
+         * Returns the group of libraries at androidx.lifecycle
+         */
+        public AndroidxLifecycleLibraryAccessors getLifecycle() {
+            return laccForAndroidxLifecycleLibraryAccessors;
+        }
+
+    }
+
+    public static class AndroidxCoreLibraryAccessors extends SubDependencyFactory {
+
+        public AndroidxCoreLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for ktx (androidx.core:core-ktx)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getKtx() {
+                return create("androidx.core.ktx");
+        }
+
+    }
+
+    public static class AndroidxFragmentLibraryAccessors extends SubDependencyFactory {
+
+        public AndroidxFragmentLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for ktx (androidx.fragment:fragment-ktx)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getKtx() {
+                return create("androidx.fragment.ktx");
+        }
+
+    }
+
+    public static class AndroidxLifecycleLibraryAccessors extends SubDependencyFactory {
+        private final AndroidxLifecycleRuntimeLibraryAccessors laccForAndroidxLifecycleRuntimeLibraryAccessors = new AndroidxLifecycleRuntimeLibraryAccessors(owner);
+
+        public AndroidxLifecycleLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Returns the group of libraries at androidx.lifecycle.runtime
+         */
+        public AndroidxLifecycleRuntimeLibraryAccessors getRuntime() {
+            return laccForAndroidxLifecycleRuntimeLibraryAccessors;
+        }
+
+    }
+
+    public static class AndroidxLifecycleRuntimeLibraryAccessors extends SubDependencyFactory {
+
+        public AndroidxLifecycleRuntimeLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for ktx (androidx.lifecycle:lifecycle-runtime-ktx)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getKtx() {
+                return create("androidx.lifecycle.runtime.ktx");
+        }
+
+    }
+
+    public static class MaplibreLibraryAccessors extends SubDependencyFactory {
+        private final MaplibreNavigationLibraryAccessors laccForMaplibreNavigationLibraryAccessors = new MaplibreNavigationLibraryAccessors(owner);
+
+        public MaplibreLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for sdk (org.maplibre.gl:android-sdk)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getSdk() {
+                return create("maplibre.sdk");
+        }
+
+        /**
+         * Returns the group of libraries at maplibre.navigation
+         */
+        public MaplibreNavigationLibraryAccessors getNavigation() {
+            return laccForMaplibreNavigationLibraryAccessors;
+        }
+
+    }
+
+    public static class MaplibreNavigationLibraryAccessors extends SubDependencyFactory {
+
+        public MaplibreNavigationLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for android (com.github.maplibre:maplibre-navigation-android)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getAndroid() {
+                return create("maplibre.navigation.android");
+        }
+
+    }
+
+    public static class Okhttp3LibraryAccessors extends SubDependencyFactory {
+        private final Okhttp3LoggingLibraryAccessors laccForOkhttp3LoggingLibraryAccessors = new Okhttp3LoggingLibraryAccessors(owner);
+
+        public Okhttp3LibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Returns the group of libraries at okhttp3.logging
+         */
+        public Okhttp3LoggingLibraryAccessors getLogging() {
+            return laccForOkhttp3LoggingLibraryAccessors;
+        }
+
+    }
+
+    public static class Okhttp3LoggingLibraryAccessors extends SubDependencyFactory {
+
+        public Okhttp3LoggingLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for interceptor (com.squareup.okhttp3:logging-interceptor)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getInterceptor() {
+                return create("okhttp3.logging.interceptor");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        private final Okhttp3VersionAccessors vaccForOkhttp3VersionAccessors = new Okhttp3VersionAccessors(providers, config);
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: agp (8.2.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAgp() { return getVersion("agp"); }
+
+            /**
+             * Returns the version associated to this alias: appcompat (1.6.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getAppcompat() { return getVersion("appcompat"); }
+
+            /**
+             * Returns the version associated to this alias: constraintlayout (2.1.4)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getConstraintlayout() { return getVersion("constraintlayout"); }
+
+            /**
+             * Returns the version associated to this alias: coreKtx (1.13.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getCoreKtx() { return getVersion("coreKtx"); }
+
+            /**
+             * Returns the version associated to this alias: fragmentKtx (1.7.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getFragmentKtx() { return getVersion("fragmentKtx"); }
+
+            /**
+             * Returns the version associated to this alias: kotlin (1.8.10)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKotlin() { return getVersion("kotlin"); }
+
+            /**
+             * Returns the version associated to this alias: lifecycleRuntimeKtx (2.7.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getLifecycleRuntimeKtx() { return getVersion("lifecycleRuntimeKtx"); }
+
+            /**
+             * Returns the version associated to this alias: maplibre (10.2.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getMaplibre() { return getVersion("maplibre"); }
+
+            /**
+             * Returns the version associated to this alias: maplibreNavigationAndroid (3.0.0)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getMaplibreNavigationAndroid() { return getVersion("maplibreNavigationAndroid"); }
+
+            /**
+             * Returns the version associated to this alias: material (1.6.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getMaterial() { return getVersion("material"); }
+
+            /**
+             * Returns the version associated to this alias: originsSdkKotlin (1.0.7)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getOriginsSdkKotlin() { return getVersion("originsSdkKotlin"); }
+
+        /**
+         * Returns the group of versions at versions.okhttp3
+         */
+        public Okhttp3VersionAccessors getOkhttp3() {
+            return vaccForOkhttp3VersionAccessors;
+        }
+
+    }
+
+    public static class Okhttp3VersionAccessors extends VersionFactory  {
+
+        private final Okhttp3LoggingVersionAccessors vaccForOkhttp3LoggingVersionAccessors = new Okhttp3LoggingVersionAccessors(providers, config);
+        public Okhttp3VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Returns the group of versions at versions.okhttp3.logging
+         */
+        public Okhttp3LoggingVersionAccessors getLogging() {
+            return vaccForOkhttp3LoggingVersionAccessors;
+        }
+
+    }
+
+    public static class Okhttp3LoggingVersionAccessors extends VersionFactory  {
+
+        public Okhttp3LoggingVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: okhttp3.logging.interceptor (4.9.3)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getInterceptor() { return getVersion("okhttp3.logging.interceptor"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for androidApplication to the plugin id 'com.android.application'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getAndroidApplication() { return createPlugin("androidApplication"); }
+
+            /**
+             * Creates a plugin provider for jetbrainsKotlinAndroid to the plugin id 'org.jetbrains.kotlin.android'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getJetbrainsKotlinAndroid() { return createPlugin("jetbrainsKotlinAndroid"); }
+
+    }
+
+}
